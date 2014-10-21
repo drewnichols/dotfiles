d1c41d703fbd872e6b6a81d85ee8ca170cb56cd3
Michael Grassotti <mgrassotti@gmail.com>
2014-10-21 00:13:18 -0400

    Update bootstrap.sh
    Add post-bundle steps to customize zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,28 +5,31 @@ xcode-select --install
 # Either way agree to xcode developer license
 sudo xcodebuild -license
 
+# Install oh-my-zsh
+curl -L http://install.ohmyz.sh | sh
+
 # Install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Run brew doctor to make sure everything is ok
 brew doctor
 
-# seems like /usr/local/bin is already in path but if not
-# brew doctor tells us we need to update bash_profile to prefer brew installs
-# echo export PATH='/usr/local/bin:$PATH' >> ~/.bash_profile
+# Clone this repo
+git clone https://github.com/mikegrassotti/dotfiles.git && cd dotfiles
 
-# Install oh-my-zsh
-curl -L http://install.ohmyz.sh | sh
+# Install libs and apps via brew bundle
+brew bundle
 
-# Install nvm
-brew install nvm
-
-# Customize zsh so nvm will work by adding this to end of .zshrc
+# Customize zsh for nvm and chruby
 echo export NVM_DIR=~/.nvm >> ~/.zshrc
 echo source $(brew --prefix nvm)/nvm.sh >> ~/.zshrc
+echo source /usr/local/opt/chruby/share/chruby/chruby.sh >> ~/.zshrc
+echo source /usr/local/opt/chruby/share/chruby/auto.sh >> ~/.zshrc
 
 # Install latest version of node and set as default
 nvm install 0.10
 nvm alias default 0.10
+
+
 
 
