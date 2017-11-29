45e4d4938d04014e7371e4184c144afa11c20f04
Drew Nichols <drew@liftforward.com>
2017-11-29 13:12:12 -0500

    ading exports to block emails

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -4,3 +4,7 @@ export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$HOME/
 # export PATH="/usr/local/heroku/bin:$PATH"
 # export PATH=$PATH:$GOPATH/bin
 # export NVM_DIR="$HOME/.nvm"
+
+export ALERT_DESTINATION='["drew@liftforward.com"]'
+export LOAN_APP_ALERT_EMAIL_TO_ARRAY=$ALERT_DESTINATION
+export NEW_USER_REGISTERED_ALERT_EMAIL=$ALERT_DESTINATION
