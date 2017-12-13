3f77464712e59ffe2e1c374761fa3ddbcb4168e5
Drew Nichols <drew@liftforward.com>
2017-12-13 12:15:23 -0500

    removing unused fiel

--- zsh/example.zsh
+++
@@ -1,7 +0,0 @@
-echo "HELLO"
-echo "running zsh_custom/example.zsh"
-# Add yourself some shortcuts to projects you often work on
-# Example:
-#
-# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
-#
