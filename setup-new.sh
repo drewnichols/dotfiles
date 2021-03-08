8eea9293caedce248974bbb458da08bb31f3a5ed
Drew Nichols <drew@liftforward.com>
2021-03-08 08:41:27 -0800

    tweaked to work on arm mac

---
+++ setup-new.sh
@@ -0,0 +1,10 @@
+arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/andrewnichols/.zprofile
+eval $(/opt/homebrew/bin/brew shellenv)
+
+cd .dotfiles
+brew Bundle
+cd ~
+ln -s .dotfiles/zshrc .zshrc
+ruby-install ruby-2.7.2
+gem install bundler
