0008916671d9469b13d1402d3d14fd1c6e28c94a
Drew Nichols <drew@liftforward.com>
2021-03-08 23:42:21 -0800

    updated directory for brew install

--- setup-new.sh
+++ setup-new.sh
@@ -1,5 +1,5 @@
 arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/andrewnichols/.zprofile
+echo 'eval $(/usr/local/bin/brew shellenv)' >> /Users/andrewnichols/.zprofile
 eval $(/opt/homebrew/bin/brew shellenv)
 
 cd .dotfiles
