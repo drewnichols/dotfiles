36071b735e2fa9c53d38b0b245f702b4c499db3c
Drew Nichols <drew@liftforward.com>
2024-01-19 17:17:22 -0800

    added settting to deal with dumb rails error

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,8 +3,22 @@
 # export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$HOME/bin"
 # export PATH="/usr/local/heroku/bin:$PATH"
 # export PATH=$PATH:$GOPATH/bin
-# export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$HOME/.nvm"
+# export PATH="./bin:$PATH"
+
+# Load version control information
+autoload -Uz vcs_info
+precmd() { vcs_info }
+
+# Format the vcs_info_msg_0_ variable
+zstyle ':vcs_info:git:*' formats 'on branch %b'
+
+# export PS1='%2~ $(vcs_info_msg_0_)%# '
+export PS1='%2~ %# '
 
 export ALERT_DESTINATION='["drew@liftforward.com"]'
 export LOAN_APP_ALERT_EMAIL_TO_ARRAY=$ALERT_DESTINATION
 export NEW_USER_REGISTERED_ALERT_EMAIL=$ALERT_DESTINATION
+
+# Fix NSCFConstantString initialize error in ruby WTF is that? 
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
